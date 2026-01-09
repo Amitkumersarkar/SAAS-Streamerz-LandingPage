dc1d283e0235ba51c22479560ee69b2a6af95f74
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-09 22:29:27 +0600

    added animation

--- src/pages/PricingPlans.jsx
+++ src/pages/PricingPlans.jsx
@@ -1,13 +1,26 @@
+import { motion } from "framer-motion";
 
 const PricingPlans = () => {
     const childVariants = {
         hidden: { opacity: 0, y: 20 },
-
+        transition: {
+            delay: 1 * 0.2,
+            duration: 0.6,
+            ease: "easOut"
+        }
     }
     return (
-        <div>
-
-        </div>
+        <section id="pricing">
+            <div className="max-w-7xl mx-auto px-4 mt-20">
+                <motion.div className="text-center mb-12 border-t border-neutral-800"
+                    initial={{ opacity: 0, y: -20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.6, ease: "easeOut" }}
+                >
+                    <h2 className=""></h2>
+                </motion.div>
+            </div>
+        </section>
     );
 };
 
