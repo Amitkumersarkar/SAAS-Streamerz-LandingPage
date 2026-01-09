36d2ea3b5b1248106454aad24e04885451dee673
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-01-09 20:30:32 +0600

    added framer motion animation

--- src/pages/KeyFeatures.jsx
+++ src/pages/KeyFeatures.jsx
@@ -1,6 +1,25 @@
 
 const KeyFeatures = () => {
-
+    const containerVariants = {
+        hidden: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.2,
+                when: "beforeChildren",
+            }
+        }
+    }
+    const featureVariants = {
+        hidden: { opacity: 0, y: 50 },
+        visible: {
+            opacity: 1,
+            y: 0,
+            transition: {
+                duration: 0.5,
+            }
+        }
+    }
     return (
         <div>
 
